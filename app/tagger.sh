d4b4ed886f147e532190ad9bcbba54316459711f
Your Name <you@example.com>
2023-02-19 20:25:57 +0200

    test commit

--- app/tagger.sh
+++ app/tagger.sh
@@ -4,16 +4,16 @@
 
 git fetch --tags
 
-LATEST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+CURRENT_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
 MAJOR=$(git tag | sort -V | tail -n 1 | cut -d '.' -f1)
 MINOR=$(git tag | sort -V | tail -n 1 | cut -d '.' -f2)
 PATCH=$(git tag | sort -V | tail -n 1 | cut -d '.' -f3)
 
-if echo $LATEST_COMMIT_MESSAGE | grep -q "MAJOR"; then
+if echo $CURRENT_COMMIT_MESSAGE | grep -q "MAJOR"; then
     MAJOR=$((MAJOR+1))
     MINOR=0
     PATCH=0
-elif echo $LATEST_COMMIT_MESSAGE | grep -q "MINOR"; then
+elif echo $CURRENT_COMMIT_MESSAGE | grep -q "MINOR"; then
     MINOR=$((MINOR+1))
     PATCH=0
 else
@@ -23,4 +23,5 @@ fi
 NEW_TAG="$MAJOR.$MINOR.$PATCH"
 echo $NEW_TAG
 git tag "$NEW_TAG"
+echo $NEW_TAG >> $GITHUB_OUTPUT
 #git push origin $NEW_TAG
